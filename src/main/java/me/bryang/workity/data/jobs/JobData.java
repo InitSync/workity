09b66b9c97cfa43d0d5c104450426d7bbdd992cc
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:27:32 -0300

    refactor(JobAction - JobData): rename some methods

--- src/main/java/me/bryang/workity/data/jobs/JobData.java
+++ src/main/java/me/bryang/workity/data/jobs/JobData.java
@@ -1,47 +1,48 @@
 package me.bryang.workity.data.jobs;
 
 import me.bryang.workity.action.JobType;
+import org.apache.commons.lang.Validate;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class JobData {
-
-    private final String jobName;
-
-    private final Map<String, BlockJobData> blockJobDataMap;
-    private final JobType activityType;
-
-    private final boolean globalStatus;
-
-
-    public JobData(String jobName, boolean globalStatus, JobType activityType,
-                   Map<String, BlockJobData> blockJobDataMap) {
-
-        this.jobName = jobName;
-
-        this.globalStatus = globalStatus;
-        this.activityType = activityType;
-        this.blockJobDataMap = blockJobDataMap;
-    }
-
-    public boolean isActivityType(JobType activityType) {
-        return this.activityType == activityType;
-    }
-
-    public String getJobName() {
-        return jobName;
-    }
-
-    public Map<String, BlockJobData> getBlockJobDataMap() {
-        return blockJobDataMap;
-    }
-
-    public BlockJobData getBlockData(String blockName) {
-        return blockJobDataMap.get(blockName);
-    }
-
-    public boolean isGlobalStatus() {
-        return globalStatus;
-    }
-
+	private final String jobName;
+	private final Map<String, BlockJobData> blockJobData;
+	private final JobType activityType;
+	private final boolean globalStatus;
+	
+	public JobData(
+		String jobName,
+		boolean globalStatus,
+		JobType activityType,
+		Map<String, BlockJobData> blockJobData
+	) {
+		Validate.notEmpty(jobName, "The job name is empty.");
+		this.jobName = jobName;
+		
+		this.globalStatus = globalStatus;
+		this.activityType = Objects.requireNonNull(activityType, "The job type is null.");
+		this.blockJobData = Objects.requireNonNull(blockJobData, "The jobData map is null.");
+	}
+	
+	public boolean isActivityType(JobType activityType) {
+		return this.activityType == activityType;
+	}
+	
+	public String getJobName() {
+		return jobName;
+	}
+	
+	public Map<String, BlockJobData> blockJobData() {
+		return blockJobData;
+	}
+	
+	public BlockJobData blockData(String blockName) {
+		return blockJobData.get(blockName);
+	}
+	
+	public boolean isGlobalStatus() {
+		return globalStatus;
+	}
 }
