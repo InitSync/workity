97c9d89aa7d4406562f6da2b64072b6733c5d64f
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:07:18 -0300

    style(BlockJobData): add some spaces to the class structure

--- src/main/java/me/bryang/workity/data/jobs/BlockJobData.java
+++ src/main/java/me/bryang/workity/data/jobs/BlockJobData.java
@@ -1,32 +1,26 @@
 package me.bryang.workity.data.jobs;
 
 public class BlockJobData {
-
-    private final int gainXP;
-    private final int gainMoney;
-
-    private final boolean enableStatus;
-
-    public BlockJobData(int gainMoney, int gainXP, boolean enableStatus) {
-
-        this.gainMoney = gainMoney;
-        this.gainXP = gainXP;
-
-        this.enableStatus = enableStatus;
-
-    }
-
-    public int getGainXP() {
-        return gainXP;
-    }
-
-
-    public int getGainMoney() {
-        return gainMoney;
-    }
-
-    public boolean isDisableStatus() {
-        return !enableStatus;
-    }
-
+	private final int gainXP;
+	private final int gainMoney;
+	private final boolean enableStatus;
+	
+	public BlockJobData(int gainMoney, int gainXP, boolean enableStatus) {
+		this.gainMoney = gainMoney;
+		this.gainXP = gainXP;
+		this.enableStatus = enableStatus;
+		
+	}
+	
+	public int getGainXP() {
+		return gainXP;
+	}
+	
+	public int getGainMoney() {
+		return gainMoney;
+	}
+	
+	public boolean isDisableStatus() {
+		return !enableStatus;
+	}
 }
