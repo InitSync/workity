db6c9df78247be6b22aabba0dd9beab2e0372067
InitSync <benjaalejandro206@gmail.com>
2022-11-19 20:04:21 -0300

    refactor(VaultHookManager): remove Logger field and use LogUtils

--- src/main/java/me/bryang/workity/manager/VaultHookManager.java
+++ src/main/java/me/bryang/workity/manager/VaultHookManager.java
@@ -1,6 +1,7 @@
 package me.bryang.workity.manager;
 
 import me.bryang.workity.Workity;
+import me.bryang.workity.utils.LogUtils;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
@@ -9,25 +10,21 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.ServicesManager;
 
 import java.util.Objects;
-import java.util.logging.Logger;
 
 public class VaultHookManager {
 	private final Workity workity;
-	private final Logger logger;
 	
 	private Economy economy;
 	private Permission permission;
 	
-	public VaultHookManager(Workity workity, Logger logger) {
+	public VaultHookManager(Workity workity) {
 		this.workity = Objects.requireNonNull(workity, "The Workity instance is null.");
-		this.logger = Objects.requireNonNull(logger, "The Logger object is null.");
 	}
 	
 	public void load() {
 		PluginManager pluginManager = Bukkit.getPluginManager();
 		if (!pluginManager.isPluginEnabled("Vault")) {
-			
-			logger.info(" Error: You need Vault to load the plugin");
+			LogUtils.error("You need Vault to load the plugin");
 			pluginManager.disablePlugin(workity);
 			return;
 		}
@@ -37,12 +34,12 @@ public class VaultHookManager {
 		RegisteredServiceProvider<Economy> economyProvider = servicesManager.getRegistration(Economy.class);
 		RegisteredServiceProvider<Permission> permissionProvider = servicesManager.getRegistration(Permission.class);
 		if (economyProvider == null) {
-			logger.info(" Error: The economy doesn't loaded correctly.");
+			LogUtils.info("The economy doesn't loaded correctly.");
 			return;
 		}
 		
 		if (permissionProvider == null) {
-			logger.info(" Error: The permission doesn't loaded correctly.");
+			LogUtils.info("The permission doesn't loaded correctly.");
 			return;
 		}
 		
