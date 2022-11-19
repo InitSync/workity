a1a36cf6da954d30f14a4c7d52ce302a263152cb
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:50:19 -0300

    refactor(WorkActionManager): make code cleanup

--- src/main/java/me/bryang/workity/manager/WorkActionManager.java
+++ src/main/java/me/bryang/workity/manager/WorkActionManager.java
@@ -8,34 +8,30 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class WorkActionManager {
-
-    private final PluginCore pluginCore;
-
-    private final List<JobManager> jobManagers = new ArrayList<>();
-
-    public WorkActionManager(PluginCore pluginCore) {
-        this.pluginCore = pluginCore;
-    }
-
-    public void loadActions() {
-        addActions(
-                new JobSetMultiplierManager(pluginCore),
-                new JobGainRewardManager(pluginCore),
-                new JobIncreaseLevelManager(pluginCore),
-                new JobAddStatsPointsManager(pluginCore),
-                new JobActionRewardsManager(pluginCore));
-
-    }
-
-    public void addActions(JobManager... newJobManagers) {
-        jobManagers.addAll(Arrays.asList(newJobManagers));
-    }
-
-    public void doActions(Player player, String jobName, String itemName, PlayerJobData playerJobData) {
-        for (JobManager jobManager : jobManagers) {
-            jobManager.doWorkAction(player, jobName, itemName, playerJobData);
-        }
-    }
+	private final List<JobManager> jobManagers;
+	
+	public WorkActionManager() {
+		this.jobManagers = new ArrayList<>();
+	}
+	
+	public void addActions(JobManager... newJobManagers) {
+		jobManagers.addAll(Arrays.asList(newJobManagers));
+	}
+	
+	public void doActions(
+		Player player,
+		String jobName,
+		String itemName,
+		PlayerJobData playerJobData
+	) {
+		jobManagers.forEach(jobManager -> {
+			jobManager.doWorkAction(player,
+				jobName,
+				itemName,
+				playerJobData);
+		});
+	}
 }
