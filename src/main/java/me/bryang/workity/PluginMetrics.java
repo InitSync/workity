79b75c0882fd82874f1eee7dfae4a1e90d550fff
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:48:05 -0300

    refactor(PluginMetrics): add final to the class header

--- src/main/java/me/bryang/workity/PluginMetrics.java
+++ src/main/java/me/bryang/workity/PluginMetrics.java
@@ -23,8 +23,7 @@ import java.util.logging.Level;
 import java.util.stream.Collectors;
 import java.util.zip.GZIPOutputStream;
 
-public class PluginMetrics {
-
+public final class PluginMetrics {
     private final Plugin plugin;
 
     private final MetricsBase metricsBase;
