b5770be52126816f857ad7995ab919e97c9ea020
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:29:24 -0300

    refactor(jobs): make code cleanup to the jobs classes

--- src/main/java/me/bryang/workity/manager/jobs/JobSetMultiplierManager.java
+++ src/main/java/me/bryang/workity/manager/jobs/JobSetMultiplierManager.java
@@ -1,52 +1,43 @@
 package me.bryang.workity.manager.jobs;
 
-import me.bryang.workity.PluginCore;
 import me.bryang.workity.data.PlayerJobData;
-import me.bryang.workity.loader.DataLoader;
 import me.bryang.workity.manager.VaultHookManager;
-import me.bryang.workity.manager.file.FileManager;
+import net.milkbowl.vault.permission.Permission;
+import net.xconfig.bukkit.config.BukkitConfigurationHandler;
 import org.bukkit.entity.Player;
 
-public class JobSetMultiplierManager implements JobManager {
+import java.util.Objects;
 
-    private final DataLoader dataLoader;
-
-    private final FileManager configFile;
-
-    private final VaultHookManager vaultHookManager;
-
-    public JobSetMultiplierManager(PluginCore pluginCore) {
-
-        this.dataLoader = pluginCore.getDataLoader();
-
-        this.configFile = pluginCore.getFilesLoader().getConfigFile();
-
-        this.vaultHookManager = pluginCore.getManagerLoader().getVaultHookManager();
-    }
-
-    @Override
-    public void doWorkAction(Player player, String jobName, String itemName, PlayerJobData playerJobData) {
-        double multiplier;
-
-
-        if (vaultHookManager.getPermission().hasGroupSupport()) {
-            if (!configFile.isInt("config.multiplier.group." +
-                    vaultHookManager.getPermission().getPrimaryGroup(player))) {
-
-                multiplier = configFile.getInt("config.multiplier.default");
-
-            } else {
-                multiplier = configFile.getInt("config.multiplier.group." +
-                        vaultHookManager.getPermission().getPrimaryGroup(player));
-            }
-        } else {
-            multiplier = configFile.getInt("config.multiplier.default");
-        }
-
-        if (dataLoader.getServerMultiplier() > 0) {
-            multiplier = multiplier + dataLoader.getServerMultiplier();
-        }
-
-        playerJobData.setMultiplier(multiplier);
-    }
+public class JobSetMultiplierManager
+implements JobManager {
+	private final BukkitConfigurationHandler configurationHandler;
+	
+	private final VaultHookManager vaultHookManager;
+	
+	public JobSetMultiplierManager(BukkitConfigurationHandler configurationHandler, VaultHookManager vaultHookManager) {
+		this.configurationHandler = Objects.requireNonNull(configurationHandler, "The BukkitConfigurationHandler object is null.");
+		this.vaultHookManager = Objects.requireNonNull(vaultHookManager, "The VaultHookManager instance is null.");
+	}
+	
+	@Override
+	public void doWorkAction(
+		Player player,
+		String jobName,
+		String itemName,
+		PlayerJobData playerJobData
+	) {
+		Permission permission = vaultHookManager.getPermission();
+		double multiplier;
+		
+		if (permission.hasGroupSupport()) {
+			String group = permission.getPrimaryGroup(player);
+			if (configurationHandler.configSection("config.yml", "config.multiplier.group." + group) == null) {
+				multiplier = configurationHandler.number("config.yml", "config.multiplier.default");
+			} else {
+				multiplier = configurationHandler.number("config.yml", "config.multiplier.group." + group);
+			}
+		} else multiplier = configurationHandler.number("config.yml", "config.multiplier.default");
+		
+		playerJobData.setMultiplier(multiplier);
+	}
 }
