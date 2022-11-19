f095fc7276015bb057f28f6defb294ebbe1a8aa9
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:46:15 -0300

    refactor(PluginLoadingActivities): code cleanup

--- src/main/java/me/bryang/workity/activites/PluginLoadingActivities.java
+++ src/main/java/me/bryang/workity/activites/PluginLoadingActivities.java
@@ -1,57 +1,54 @@
 package me.bryang.workity.activites;
 
-import me.bryang.workity.PluginCore;
-import me.bryang.workity.Workity;
-import me.bryang.workity.data.PlayerData;
 import me.bryang.workity.data.PlayerJobData;
 import me.bryang.workity.database.Database;
 import me.bryang.workity.loader.DataLoader;
-import me.bryang.workity.manager.file.FileManager;
 import me.bryang.workity.utils.MathLevelsUtils;
+import net.xconfig.bukkit.config.BukkitConfigurationHandler;
+import org.bukkit.Bukkit;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 public class PluginLoadingActivities implements Activities {
-
-    private final FileManager configFile;
-    private final DataLoader dataLoader;
-
-    private final Database database;
-
-    private final Workity workity;
-
-    public PluginLoadingActivities(PluginCore pluginCore) {
-        this.workity = pluginCore.getPlugin();
-        this.configFile = pluginCore.getFilesLoader().getConfigFile();
-        this.database = pluginCore.getDatabaseLoader().getDatabase();
-        this.dataLoader = pluginCore.getDataLoader();
-    }
-
-    public void loadTask() {
-
-        if (database.getPlayerList() == null) {
-            workity.getLogger().info(" Thanks for using my plugin, don't forget check config.yml");
-            return;
-        }
-
-
-        for (String stringToUUID : database.getPlayerList()) {
-            UUID playerUniqueId = UUID.fromString(stringToUUID);
-
-            dataLoader.createPlayerJob(playerUniqueId);
-
-            PlayerData playerData = dataLoader.getPlayerJob(playerUniqueId);
-
-            for (String jobName : database.getPlayerJobs(playerUniqueId)) {
-                PlayerJobData playerJobData = new PlayerJobData(jobName);
-
-                playerJobData.setLevel(database.getJobIntData(playerUniqueId, jobName, "xp"));
-                playerJobData.setLevel(database.getJobIntData(playerUniqueId, jobName, "level"));
-
-                playerJobData.setMaxXP(
-                        MathLevelsUtils.calculateNumber(configFile.getString("config.formula.max-xp"), 1));
-                playerData.putJob(jobName, playerJobData);
-            }
-        }
-    }
+	private final BukkitConfigurationHandler configurationHandler;
+	private final DataLoader dataLoader;
+	private final Database database;
+	
+	public PluginLoadingActivities(
+		BukkitConfigurationHandler configurationHandler,
+		Database database,
+		DataLoader dataLoader
+	) {
+		this.configurationHandler = Objects.requireNonNull(configurationHandler, "The BukkitConfigurationHandler instance is null.");
+		this.database = Objects.requireNonNull(database, "The Database instance is null.");
+		this.dataLoader = Objects.requireNonNull(dataLoader, "The DataLoader object is null.");
+	}
+	
+	public void loadTask() {
+		List<String> playersList = database.playersList();
+		if (playersList == null) {
+			Bukkit.getLogger().info("Thanks for using my plugin, don't forget check config.yml");
+			return;
+		}
+		
+		playersList.forEach(player -> {
+			UUID playerId = UUID.fromString(player);
+			dataLoader.createPlayerJob(playerId);
+			
+			for (String jobName : database.playerJobs(playerId)) {
+				PlayerJobData playerJobData = new PlayerJobData(jobName);
+				playerJobData.setLevel(database.jobIntData(playerId,
+					jobName,
+					"xp"));
+				playerJobData.setLevel(database.jobIntData(playerId,
+					jobName,
+					"level"));
+				
+				playerJobData.setMaxExp(MathLevelsUtils.calculateNumber(configurationHandler.text("config.yml", "config.formula.max-xp"), 1));
+				dataLoader.playerJob(playerId).putJob(jobName, playerJobData);
+			}
+		});
+	}
 }
