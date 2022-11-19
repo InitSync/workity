3e04074f58373977f0a254d0024218bbf4c55d60
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:41:32 -0300

    refactor(PlayerData): rename some methods

--- src/main/java/me/bryang/workity/data/PlayerData.java
+++ src/main/java/me/bryang/workity/data/PlayerData.java
@@ -4,34 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PlayerData {
-
-    private final Map<String, PlayerJobData> jobDataMap = new HashMap<>();
-
-    public void addJob(String jobName) {
-        jobDataMap.put(jobName, new PlayerJobData(jobName));
-    }
-
-    public void putJob(String jobName, PlayerJobData playerJobData) {
-        jobDataMap.put(jobName, playerJobData);
-    }
-
-    public PlayerJobData getJob(String jobName) {
-        return jobDataMap.get(jobName);
-    }
-
-    public void removeJob(String jobName) {
-        jobDataMap.remove(jobName);
-    }
-
-    public boolean hasTheJob(String jobName) {
-        return jobDataMap.containsKey(jobName);
-    }
-
-    public int getJobSize() {
-        return jobDataMap.size();
-    }
-
-    public Map<String, PlayerJobData> getJobsMap() {
-        return jobDataMap;
-    }
+	private final Map<String, PlayerJobData> jobDataMap = new HashMap<>();
+	
+	public void addJob(String jobName) {
+		jobDataMap.put(jobName, new PlayerJobData(jobName));
+	}
+	
+	public void putJob(String jobName, PlayerJobData playerJobData) {
+		jobDataMap.put(jobName, playerJobData);
+	}
+	
+	public PlayerJobData job(String jobName) {
+		return jobDataMap.get(jobName);
+	}
+	
+	public void removeJob(String jobName) {
+		jobDataMap.remove(jobName);
+	}
+	
+	public boolean hasJob(String jobName) {
+		return jobDataMap.containsKey(jobName);
+	}
+	
+	public int getJobSize() {
+		return jobDataMap.size();
+	}
+	
+	public Map<String, PlayerJobData> jobData() {
+		return jobDataMap;
+	}
 }
