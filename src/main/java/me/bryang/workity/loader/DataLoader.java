8e91304936ae7a3a9e231a2358f8770086472ed1
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:16:34 -0300

    refactor(DataLoader): remove Loader interface implementation, move Maps initialization to constructor and rename methods

--- src/main/java/me/bryang/workity/loader/DataLoader.java
+++ src/main/java/me/bryang/workity/loader/DataLoader.java
@@ -8,52 +8,33 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public class DataLoader implements Loader {
-
-    private final Map<UUID, PlayerData> playerJobDataMap = new HashMap<>();
-    private final Map<String, JobData> jobDataMap = new HashMap<>();
-
-    private final Workity workity;
-    double serverMultiplier;
-
-    public DataLoader(Workity workity) {
-        this.workity = workity;
-    }
-
-    @Override
-    public void load() {
-        serverMultiplier = 0;
-        workity.getLogger().info(" Data loaded.");
-
-    }
-
-    public void createPlayerJob(UUID uuid) {
-        playerJobDataMap.put(uuid, new PlayerData());
-    }
-
-    public PlayerData getPlayerJob(UUID uuid) {
-        return playerJobDataMap.get(uuid);
-    }
-
-    public JobData getJob(String jobName) {
-        return jobDataMap.get(jobName);
-    }
-
-    public boolean jobExists(String jobName) {
-        return jobDataMap.get(jobName) != null;
-    }
-
-    public double getServerMultiplier() {
-        return serverMultiplier;
-    }
-
-    public void setServerMultiplier(double serverMultiplier) {
-        this.serverMultiplier = serverMultiplier;
-    }
-
-    public Map<String, JobData> getJobDataMap() {
-        return jobDataMap;
-    }
-
-
+public class DataLoader {
+	
+	private final Map<UUID, PlayerData> playerJobData;
+	private final Map<String, JobData> jobData;
+	
+	public DataLoader() {
+		this.playerJobData = new HashMap<>();
+		this.jobData = new HashMap<>();
+	}
+	
+	public void createPlayerJob(UUID uuid) {
+		playerJobData.put(uuid, new PlayerData());
+	}
+	
+	public PlayerData playerJob(UUID uuid) {
+		return playerJobData.get(uuid);
+	}
+	
+	public JobData job(String jobName) {
+		return jobData.get(jobName);
+	}
+	
+	public boolean exists(String jobName) {
+		return jobData.get(jobName) != null;
+	}
+	
+	public Map<String, JobData> jobData() {
+		return jobData;
+	}
 }
