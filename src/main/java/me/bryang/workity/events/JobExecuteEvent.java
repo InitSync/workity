30ba6ea9fb9890d30b6956f71099f7629a747ad1
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:28:55 -0300

    refactor(JobExecuteEvent): remove some spaces and getHandlerList() method, and rename some methods

--- src/main/java/me/bryang/workity/events/JobExecuteEvent.java
+++ src/main/java/me/bryang/workity/events/JobExecuteEvent.java
@@ -1,41 +1,32 @@
 package me.bryang.workity.events;
 
-
 import me.bryang.workity.action.JobAction;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class JobExecuteEvent extends Event {
-
-    private static final HandlerList handlerList = new HandlerList();
-
-    private final UUID targetUniqueId;
-
-    private final JobAction jobAction;
-
-    public JobExecuteEvent(UUID targetUniqueId, JobAction jobAction) {
-        this.jobAction = jobAction;
-        this.targetUniqueId = targetUniqueId;
-    }
-
-    public static HandlerList getHandlerList() {
-        return handlerList;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return handlerList;
-    }
-
-    public UUID getTarget() {
-        return targetUniqueId;
-    }
-
-    public JobAction getAction() {
-        return jobAction;
-    }
-
-
+	private final HandlerList handlerList = new HandlerList();
+	private final UUID targetId;
+	private final JobAction jobAction;
+	
+	public JobExecuteEvent(UUID targetId, JobAction jobAction) {
+		this.jobAction = Objects.requireNonNull(jobAction, "The job action is null.");
+		this.targetId = Objects.requireNonNull(targetId, "The target uuid is null.");
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlerList;
+	}
+	
+	public UUID targetId() {
+		return targetId;
+	}
+	
+	public JobAction jobAction() {
+		return jobAction;
+	}
 }
