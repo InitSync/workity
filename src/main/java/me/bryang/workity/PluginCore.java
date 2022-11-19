5e164d63ecd104e0e4db1f06e9fb61eb1e2c139d
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:45:23 -0300

    refactor(PluginCore): apply interface changes, and initialized plugin components

--- src/main/java/me/bryang/workity/PluginCore.java
+++ src/main/java/me/bryang/workity/PluginCore.java
@@ -1,67 +1,90 @@
 package me.bryang.workity;
 
+import me.bryang.workity.activites.JobsLoadingActivity;
+import me.bryang.workity.activites.MetricsLoadingActivities;
+import me.bryang.workity.activites.PluginLoadingActivities;
 import me.bryang.workity.database.Database;
-import me.bryang.workity.loader.*;
-import me.bryang.workity.loader.command.CommandsLoader;
+import me.bryang.workity.loader.DataLoader;
+import me.bryang.workity.manager.ActivitiesManager;
+import me.bryang.workity.manager.FileConverter;
+import me.bryang.workity.manager.VaultHookManager;
+import me.bryang.workity.manager.WorkActionManager;
+import me.bryang.workity.manager.jobs.*;
+import me.bryang.workity.services.DatabaseService;
+import me.bryang.workity.services.LoaderService;
+import me.bryang.workity.services.ManagerService;
+import net.xconfig.bukkit.config.BukkitConfigurationHandler;
+import net.xconfig.bukkit.config.BukkitConfigurationModel;
+import net.xconfig.services.ConfigurationService;
+import org.bukkit.Bukkit;
 
-public class PluginCore implements Core {
-
-    private final Workity plugin;
-
-    private FilesLoader filesLoader;
-    private DataLoader dataLoader;
-    private ManagerLoader managerLoader;
-    private DatabaseLoader databaseLoader;
-
-    public PluginCore(Workity plugin) {
-        this.plugin = plugin;
-    }
-
-    @Override
-    public void init() {
-
-        filesLoader = new FilesLoader(plugin);
-        dataLoader = new DataLoader(plugin);
-
-        databaseLoader = new DatabaseLoader(this);
-        managerLoader = new ManagerLoader(this);
-
-        initLoaders(
-                filesLoader,
-                databaseLoader,
-                dataLoader,
-                new CommandsLoader(this),
-                new ListenersLoader(this),
-                managerLoader
-        );
-
-    }
-
-
-    private void initLoaders(Loader... loaders) {
-        for (Loader loader : loaders) {
-            loader.load();
-        }
-    }
-
-
-    public FilesLoader getFilesLoader() {
-        return filesLoader;
-    }
-
-    public DataLoader getDataLoader() {
-        return dataLoader;
-    }
-
-    public ManagerLoader getManagerLoader() {
-        return managerLoader;
-    }
-
-    public DatabaseLoader getDatabaseLoader() {
-        return databaseLoader;
-    }
-
-    public Workity getPlugin() {
-        return plugin;
-    }
+public class PluginCore
+implements Core {
+	private BukkitConfigurationModel configurationManager;
+	private BukkitConfigurationHandler configurationHandler;
+	private DataLoader dataLoader;
+	private Database database;
+	private ActivitiesManager activitiesManager;
+	private FileConverter fileConverter;
+	private VaultHookManager vaultHookManager;
+	private WorkActionManager workActionManager;
+	
+	public PluginCore(Workity plugin) {
+		this.configurationManager = ConfigurationService.bukkitManager(plugin);
+		this.configurationHandler = ConfigurationService.bukkitHandler(configurationManager);
+		this.dataLoader = LoaderService.dataLoader();
+		this.database = DatabaseService.playerFilesDatabase(configurationHandler);
+		this.activitiesManager = ManagerService.activitiesManager();
+		this.fileConverter = ManagerService.fileConverter(configurationManager, configurationHandler);
+		this.vaultHookManager = ManagerService.vaultHookManager(plugin, Bukkit.getLogger());
+		this.workActionManager = ManagerService.workActionManager(this);
+	}
+	
+	@Override
+	public void enable() {
+		configurationManager.create("",
+			"config.yml",
+			"messages.yml",
+			"players.yml");
+		configurationManager.create("jobs",
+			"builder.yml",
+			"hunter.yml",
+			"miner.yml",
+			"woodcutter.yml");
+		configurationManager.load("config.yml",
+			"messages.yml",
+			"players.yml",
+			"builder.yml",
+			"hunter.yml",
+			"miner.yml",
+			"woodcutter.yml");
+		
+		vaultHookManager.load();
+		workActionManager.addActions(new JobActionRewardsManager(configurationHandler, vaultHookManager),
+			new JobAddStatsPointsManager(database, dataLoader),
+			new JobGainRewardManager(dataLoader,
+				configurationHandler,
+				database,
+				vaultHookManager),
+			new JobIncreaseLevelManager(configurationHandler, database),
+			new JobSetMultiplierManager(configurationHandler, vaultHookManager));
+		
+		activitiesManager.loadActivities(new JobsLoadingActivity(dataLoader, fileConverter),
+			new MetricsLoadingActivities(configurationHandler),
+			new PluginLoadingActivities(configurationHandler, database, dataLoader));
+	}
+	
+	@Override
+	public void disable() {
+		if (configurationManager != null) configurationManager = null;
+		if (configurationHandler != null) configurationHandler = null;
+		
+		if (database != null) database = null;
+		if (dataLoader != null) dataLoader = null;
+		
+		if (activitiesManager != null) activitiesManager = null;
+		if (fileConverter != null) fileConverter = null;
+		if (vaultHookManager != null) vaultHookManager = null;
+		if (workActionManager != null) workActionManager = null;
+	}
 }
