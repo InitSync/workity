89d700b61476c5ac192e7ff4425831d75c50e7b9
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:44:17 -0300

    refactor(PlayerJobData): rename methods

--- src/main/java/me/bryang/workity/data/PlayerJobData.java
+++ src/main/java/me/bryang/workity/data/PlayerJobData.java
@@ -4,82 +4,80 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PlayerJobData {
-
-    private final String jobName;
-    private final Map<String, Integer> itemData = new HashMap<>();
-    private int globalStats = 0;
-    private double xpPoints = 0;
-    private int maxXP = 0;
-    private int level = 1;
-
-    private double multiplier = 0;
-
-    public PlayerJobData(String jobName) {
-        this.jobName = jobName;
-    }
-
-    public String getName() {
-        return jobName;
-    }
-
-    public void removeXPPoints() {
-        xpPoints--;
-    }
-
-    public double getXpPoints() {
-        return xpPoints;
-    }
-
-    public void addLevel() {
-        level++;
-    }
-
-    public void removeLevel() {
-        level--;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public void setXPPoints(double xpPoints) {
-        this.xpPoints = xpPoints;
-    }
-
-    public int getMaxXP() {
-        return maxXP;
-    }
-
-    public void setMaxXP(int maxXP) {
-        this.maxXP = maxXP;
-    }
-
-    public Map<String, Integer> getJobData() {
-        return itemData;
-    }
-
-    public void addGlobalStats() {
-        globalStats++;
-    }
-
-    public int getGlobalStats() {
-        return globalStats;
-    }
-
-    public void setGlobalStats(int globalStats) {
-        this.globalStats = globalStats;
-    }
-
-    public double getMultiplier() {
-        return multiplier;
-    }
-
-    public void setMultiplier(double multiplier) {
-        this.multiplier = multiplier;
-    }
+	private final String jobName;
+	private final Map<String, Integer> itemData = new HashMap<>();
+	private int globalStats;
+	private double expPoints;
+	private int maxXP;
+	private int level;
+	private double multiplier;
+	
+	public PlayerJobData(String jobName) {
+		this.jobName = jobName;
+	}
+	
+	public String getName() {
+		return jobName;
+	}
+	
+	public void decreaseExpPoints() {
+		expPoints--;
+	}
+	
+	public double getExpPoints() {
+		return expPoints;
+	}
+	
+	public void increaseLevel() {
+		level++;
+	}
+	
+	public void decreaseLevel() {
+		level--;
+	}
+	
+	public int getLevel() {
+		return level;
+	}
+	
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	public void setExpPoints(double expPoints) {
+		this.expPoints = expPoints;
+	}
+	
+	public int getMaxExp() {
+		return maxXP;
+	}
+	
+	public void setMaxExp(int maxExp) {
+		this.maxXP = maxExp;
+	}
+	
+	public Map<String, Integer> jobData() {
+		return itemData;
+	}
+	
+	public void increaseGlobalStats() {
+		globalStats++;
+	}
+	
+	public int getGlobalStats() {
+		return globalStats;
+	}
+	
+	public void setGlobalStats(int globalStats) {
+		this.globalStats = globalStats;
+	}
+	
+	public double getMultiplier() {
+		return multiplier;
+	}
+	
+	public void setMultiplier(double multiplier) {
+		this.multiplier = multiplier;
+	}
 }
 
