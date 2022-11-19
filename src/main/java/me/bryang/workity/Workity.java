7eb9113d2c6dc8aa3f67a4ff5982d02af4644a8d
InitSync <benjaalejandro206@gmail.com>
2022-11-19 18:51:49 -0300

    chore(Workity - hunter.yml): updated files

--- src/main/java/me/bryang/workity/Workity.java
+++ src/main/java/me/bryang/workity/Workity.java
@@ -2,22 +2,41 @@ package me.bryang.workity;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Workity extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-
-        PluginCore pluginCore = new PluginCore(this);
-        pluginCore.init();
-
-        getLogger().info(" Created by " + getDescription().getAuthors().get(0));
-        getLogger().info(" You are using the version " + getDescription().getVersion() + ".");
-        getLogger().info("Click to support: http://discord.devblook.team/");
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+public final class Workity
+extends JavaPlugin {
+	private static Workity instance;
+	
+	private PluginCore core;
+	
+	public Workity() {
+		instance = this;
+		
+		core = new PluginCore(this);
+	}
+	
+	public static Workity instance() {
+		if (instance == null) throw new IllegalStateException("The Workity instance is null.");
+		
+		return instance;
+	}
+	
+	@Override
+	public void onEnable() {
+		core.enable();
+		
+		getLogger().info(" Created by " + getDescription().getAuthors().get(0));
+		getLogger().info(" You are using the version " + getDescription().getVersion() + ".");
+		getLogger().info("Click to support: http://discord.devblook.team/");
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		if (core != null) {
+			core.disable();
+			core = null;
+		}
+		
+		if (instance != null) instance = null;
+	}
 }
