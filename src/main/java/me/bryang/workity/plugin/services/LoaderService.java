545b173ef01f6f5f3a8730fcde361f090c9af982
InitSync <benjaalejandro206@gmail.com>
2022-11-26 10:31:54 -0300

    feat(LoaderService): add loader() method of Loader type

--- src/main/java/me/bryang/workity/plugin/services/LoaderService.java
+++ src/main/java/me/bryang/workity/plugin/services/LoaderService.java
@@ -1,11 +1,16 @@
 package me.bryang.workity.plugin.services;
 
 import me.bryang.workity.Workity;
+import me.bryang.workity.plugin.Loader;
 import me.bryang.workity.plugin.loader.CommandLoader;
 import me.bryang.workity.plugin.loader.DataLoader;
 import me.bryang.workity.plugin.loader.EventLoader;
 
 public interface LoaderService {
+	static Loader loader(Workity plugin) {
+		return new Loader(plugin);
+	}
+	
 	static CommandLoader.Builder commandLoader(Workity plugin) {
 		return new CommandLoader.Builder(plugin);
 	}
